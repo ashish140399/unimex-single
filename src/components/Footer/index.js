d516695b4d708a146f27bbd23998643aa7ac2b3f
ashish140399 <ashish140399@gmail.com>
2022-01-06 17:57:48 +0530

    hqbg image and links

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -10,16 +10,16 @@ export default function Footer() {
         <p>Unimex 2022</p>
         <div className="d-flex gap-2">
           <div className="icon">
-          <i className="fab fa-twitter"></i>
+          <a target="_blank" href="https://twitter.com/UniMex_Network"><i class="fab fa-twitter"></i></a>
           </div>
           <div className="icon">
-            <i className="fab fa-facebook-f"></i>
+          <a target="_blank" href="https://t.me/UniMexNetwork"><i class="fab fa-telegram-plane"></i></a>
           </div>
           <div className="icon">
-            <i className="fab fa-facebook-f"></i>
+          <a target="_blank" href="https://discord.gg/Kj4P8Q8"><i class="fab fa-discord"></i></a>
           </div>
           <div className="icon">
-            <i className="fab fa-facebook-f"></i>
+          <a target="_blank" href="https://unimex.medium.com/"><i class="fab fa-medium-m"></i></a>
           </div>
         </div>
       </Container>
